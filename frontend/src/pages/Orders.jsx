c7ed19238a70590d21a8717717ac7dccc77769d9
Mohsin Miyaji <mohsin.kmiyaji@gmail.com>
2023-04-28 17:01:51 +0530

    CHANGELOG 2.0     - added orders page, company page and Purchase page     - next steps are to connect backend with the frontend     - if time permits will add a category bar and search bar

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,5 +1,47 @@
 import "./Orders.css";
 
 export default function Orders() {
-  return <div>Orders</div>;
+  return (
+    <section className="container order-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Product name</th>
+            <th>Company Name</th>
+            <th>Quantity</th>
+            <th>status</th>
+            <th>-</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Chair</td>
+            <td>Furniture.co</td>
+            <td>20</td>
+            <td className="status-block">Processing</td>
+            <button>deliver</button>
+          </tr>
+          <tr>
+            <td>mobile</td>
+            <td>Electronic.co</td>
+            <td>25</td>
+            <td
+              className="status-block"
+              style={{ backgroundColor: "lightgreen" }}
+            >
+              Delivered
+            </td>
+            <button>deliver</button>
+          </tr>
+          <tr>
+            <td>Chair</td>
+            <td>Furniture.co</td>
+            <td>20</td>
+            <td className="status-block">Processing</td>
+            <button>deliver</button>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
 }
--- frontend/src/pages/Purchase.jsx
+++ frontend/src/pages/Purchase.jsx
@@ -1,3 +1,77 @@
+// react dom
+import { useLoaderData } from "react-router-dom";
+
+// styles
+import "./Purchase.css";
+// REACT
+import { useState } from "react";
+export async function companyLoader({ params }) {
+  const id = params.id;
+  return { id };
+}
+
 export default function Purchase() {
-  return <div>Purchase</div>;
+  const { id } = useLoaderData();
+  const [sell, setSell] = useState("");
+  const [companyName, setCompanyName] = useState("Furniture");
+  const sellItem = () => {
+    console.log(sell, "add this this");
+  };
+  return (
+    <section className="container company-container">
+      <div className="company-details flow" style={{ "--flow-spacer": "4rem" }}>
+        <h1>{companyName} Company Name</h1>
+        <p>item no {id}</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          mollitia itaque alias numquam. Laudantium quaerat consectetur
+          praesentium, laborum alias distinctio fugit voluptas quam iure magni,
+          excepturi dicta adipisci neque ducimus! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Adipisci ab officiis, aliquam labore hic
+          quas quae quo incidunt cupiditate totam voluptatibus, praesentium
+          quaerat ea in tempore vel qui impedit quibusdam?
+        </p>
+        <div className="company-options">
+          <label>
+            <input
+              value={sell}
+              onChange={(e) => setSell(e.target.value)}
+              type="number"
+              min="1"
+            />
+            <button onClick={sellItem}>Buy</button>
+          </label>
+        </div>
+      </div>
+      <div className="company">
+        <div
+          onClick={() => setCompanyName("Furniture")}
+          className="company-image"
+        >
+          Furniture
+        </div>
+        <div
+          onClick={() => setCompanyName("Electronics")}
+          className="company-image"
+        >
+          Electronics
+        </div>
+        <div onClick={() => setCompanyName("Books")} className="company-image">
+          Books
+        </div>
+        <div
+          onClick={() => setCompanyName("Clothes")}
+          className="company-image"
+        >
+          Clothes
+        </div>
+        <div onClick={() => setCompanyName("Sports")} className="company-image">
+          Sports
+        </div>
+        <div onClick={() => setCompanyName("Travel")} className="company-image">
+          Travel
+        </div>
+      </div>
+    </section>
+  );
 }
